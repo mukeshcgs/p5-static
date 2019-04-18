41de54b0c7b4db30e2396177568a0e2cd9778883
Thankar <mukesh.thankar@MBICWW-DMX00031.local>
2019-04-18 16:19:00 +0530

    grunt task added

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,38 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         watch: {
             sass: {
                 files: ['src/sass/**/*.{scss,sass}', 'src/sass/_partials/**/*.{scss,sass}'],
-                tasks: ['sass:Sketches']
+                tasks: ['sass:main']
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    livereload: true
+                }
+            },
+            images:{
+                files:['src/img'],
+                task:['imagemin', 'copy']
             },
             livereload: {
-                files: ['Sketches/*.html', 'Sketches/*.php', 'Sketches/js/**/*.{js,json}', 'Sketches/css/*.css', 'Sketches/img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+                files: [
+                    'dist/*.html',
+                    'dist/js/**/*.{js,json}',
+                    'dist/css/*.css',
+                    'dist/img/**/*.{png,jpg,jpeg,gif,webp,svg}',
+                    'Sketches/*.html',
+                    'Sketches/*.php',
+                    'Sketches/js/**/*.{js,json}',
+                    'Sketches/css/*.css',
+                    'Sketches/img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
                 options: {
                     livereload: true
                 }
@@ -19,26 +43,90 @@ module.exports = function(grunt) {
                 sourceMap: true,
                 outputStyle: 'compressed'
             },
-            Sketches: {
+            main: {
                 files: {
-                    'Sketches/css/bootstrap.min.css': 'src/sass/bootstrap.scss'
+                    'dist/css/main.min.css': 'src/sass/main.scss'
                 }
             }
         },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/js/main.min.js': ['src/js/index.js']
+                }
+            }
+        },
+        browserify: {
+            dist: {
+                files: {
+                    // destination for transpiled js : source js
+                    'dist/js/main.js': 'src/js/index.js'
+                },
+                options: {
+                    transform: [['babelify', { presets: "es2015" }]],
+                    browserifyOptions: {
+                        debug: true
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['src/js/index.js',],
+                dest: 'dist/js/main.js',
+            },
+        },
+        imagemin: {
+            dynamic: {
+                options: { // Target options 
+                    optimizationLevel: 3,
+                    cache: false
+                },
+                files: [{
+                    expand: true, // Enable dynamic expansion 
+                    cwd: 'src/img', // app matches are relative to this path 
+                    src: ['*.{ jpg, gif, png}'], // Actual patterns to match 
+                    dest: 'dist/img' // Destination path prefix 
+                }]
+            }
+        },
+        copy: {
+            // making working assets copy
+            main: {
+                files: [{
+                    expand: false,
+                    flatten: false,
+                    src: ['src/img/*.{ jpg, gif, png}'],
+                    dest: 'dist/img',
+                    filter: 'isFile'
+                },]
+            }
+        },
+
         browserSync: {
             dev: {
                 bsFiles: {
-                    src: ['Sketches/css/*.css', 'Sketches/**/*.js', 'Sketches/*.html']
+                    src: ['dist/css/*.css', 'dist/**/*.js', 'browserify:dist', 'dist/*.html']
                 },
                 options: {
                     watchTask: true,
-                    server: './'
+                    server: 'dist/'
                 }
             }
         },
     });
-    grunt.registerTask('default', ['browserSync', 'watch', 'sass:Sketches']);
+    grunt.registerTask('default', ['browserSync', 'watch', 'uglify', 'sass:main', 'copy']);
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
 };
