e8a7fe1693c281cbff77c6522821e77f0a3afe57
Mukesh <mukeshcgs@gmail.com>
2019-05-13 00:23:10 +0530

    loop changes

--- Sketches/sketch-06_lines/sketch.js
+++ Sketches/sketch-06_lines/sketch.js
@@ -20,31 +20,34 @@ function randPointOnLine(lineStart, lineEnd) {
   stroke(255, 0, 0, 50)
   let ransRadius = [];
 
-  for (let i = 0; i < randPosArray.length; i++) {
-    for (let j = 0; j < randPosArray.length + 1; j++) {
-      let sum = randPosArray[i] - randPosArray[j]
-      ransRadius.push(Math.abs(sum) * 2);
-      ellipse(randPosX, lineStart, sum, sum)
-    }
+  // for (let i = 0; i < randPosArray.length; i++) {
+  for (let j = 0; j < randPosArray.length; j++) {
+    let sum = randPosArray[0] - randPosArray[j]
+    ransRadius.push(Math.abs(sum) * 2);
+    ellipse(randPosX, lineStart, sum * 2, sum * 2)
   }
+  // }
 
   // let ransRadius = floor(random(randPosArray));
   // ellipse(randPosX, lineStart, ransRadius / 2, ransRadius / 2)
 
   pop()
-  console.log(ransRadius);
+  // console.log(ransRadius);
 
-  let thisVertex = pointOnCircle(randPosX, lineStart, random(ransRadius), random(TWO_PI));
+  let thisVertex = pointOnCircle(randPosX, lineStart, 100, random(TWO_PI));
   line(randPosX, lineStart, thisVertex.x, thisVertex.y)
 }
 function draw() {
   background(0, 0, 0)
   //DRAW GRID
-  // grids(30);
+  grids(100);
+  console.log(mouseX);
 
   stroke(255);
   for (let i = 0; i < lineCount; i++) {
     let randPosY = random(100, winH - 100);
+    console.log(randPosY);
+
     randPosArray.push(floor(randPosY));
     // console.log(randPosArray);
     line(0, randPosY, winW, randPosY)
